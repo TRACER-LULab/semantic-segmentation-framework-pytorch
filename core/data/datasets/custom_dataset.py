4489460206f69b056b691b19ae4bf88a9eec762b
Kolton Musgrove <KoltonGMusgrove@gmail.com>
2021-08-08 23:47:57 -0500

    update scripts to refelect changes in external libraries

--- core/data/datasets/custom_dataset.py
+++ core/data/datasets/custom_dataset.py
@@ -18,22 +18,26 @@ class CustomDataset(BaseDataset):
 
     """
 
-    CLASSES = ['background', 'foreground']
+    CLASSES = ["background", "foreground"]
 
     def __init__(
-            self,
-            images_dir,
-            masks_dir,
-            classes=None,
-            transforms=None,
-            preprocessing=None,
-            split='train',
+        self,
+        images_dir,
+        masks_dir,
+        classes=None,
+        transforms=None,
+        preprocessing=None,
+        split="train",
     ):
-        pre_image_names = os.listdir(self.images_dir)
-        self.ids = [f.split(".")[0] for f in preimage_names[0:10]]
+        pre_image_names = os.listdir(images_dir)
+        self.ids = [f.split(".")[0] for f in pre_image_names[0:10]]
         self.ids = os.listdir(images_dir)
-        self.images_fps = [os.path.join(images_dir, image_id+".jpg") for image_id in self.ids]
-        self.masks_fps = [os.path.join(masks_dir, image_id+".png") for image_id in self.ids]
+        self.images_fps = [
+            os.path.join(images_dir, image_id + ".jpg") for image_id in self.ids
+        ]
+        self.masks_fps = [
+            os.path.join(masks_dir, image_id + ".png") for image_id in self.ids
+        ]
 
         # convert str names to class values on masks
         self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
@@ -51,18 +55,18 @@ class CustomDataset(BaseDataset):
 
         # extract certain classes from mask (e.g. cars)
         masks = [(mask == 255) for v in self.class_values]
-        mask = np.stack(masks, axis=-1).astype('float')
+        mask = np.stack(masks, axis=-1).astype("float")
 
         # apply augmentations
         if self.augmentation:
             sample = self.augmentation(image=image, mask=mask)
-            image, mask = sample['image'], sample['mask']
+            image, mask = sample["image"], sample["mask"]
 
         # apply preprocessing
         if self.preprocessing:
             sample = self.preprocessing(image=image, mask=mask)
-            image, mask = sample['image'], sample['mask']
-        if self.split == 'test':
+            image, mask = sample["image"], sample["mask"]
+        if self.split == "test":
             return image, mask, os.path.basename(self.images_fps[i])
         return image, mask
 
--- core/utils/imports.py
+++ core/utils/imports.py
@@ -1,10 +1,9 @@
 import torch
 
-if torch._six.PY3:
+if torch._six.PY37:
     import importlib
     import importlib.util
     import sys
-
 
     # from https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
     def import_file(module_name, file_path, make_importable=False):
@@ -14,6 +13,8 @@ if torch._six.PY3:
         if make_importable:
             sys.modules[module_name] = module
         return module
+
+
 else:
     import imp
 
